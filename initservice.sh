b7b325a2351dd154bea854d4ef49eaa2da574ba6
rednexela1941 <40923759+rednexela1941@users.noreply.github.com>
2020-10-03 01:30:31 -0400

    init service

--- initservice.sh
+++ initservice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -s ~/brightness/brightness.service /etc/systemd/system/brightness.service
+sudo ln -s ~/brightness/brightness.service /etc/systemd/system/brightness.service
 
 systemctl enable brightness.service
 systemctl start brightness.service 
